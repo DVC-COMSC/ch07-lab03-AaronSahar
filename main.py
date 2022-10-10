7db3950e61505e71030d9b889f6a1ac06506cdda
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 16:50:12 -0700

    test

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-
-# ******************************
-# Make your Code
-# ******************************
+numbers = []
+for i in range(5):
+    numbers.append(int(input()))
+average = sum(numbers) / len(numbers)
+i=0
+while i < len(numbers):
+    if numbers[i] <= average:
+        numbers.remove(numbers[i])
+        i-=1
+    i+=1
+for i in range(len(numbers)):
+    print (numbers[i], end=' ')
